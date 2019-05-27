d7b8d2721494ad71bb8e582df10c27fde4e3322c
Marcus Thomas <th@gso-koeln.de>
2019-05-27 20:36:15 +0200

    Lösungsvorlage für die Zahlenpyramide

---
+++ Lösungen/Zahlenpyramide/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zahlenpyramide
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
